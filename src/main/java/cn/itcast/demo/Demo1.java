070277336613953a6f402bcc4023091a6cba22d2
zyt123 <15591935052@163.com>
2019-06-08 19:36:31 +0800

    把本地工程添加到git

---
+++ src/main/java/cn/itcast/demo/Demo1.java
@@ -0,0 +1,12 @@
+package cn.itcast.demo;
+
+public class Demo1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello,git");
+        System.out.println("Hello,git");
+        System.out.println("Hello,git");
+        System.out.println("Hello,git");
+        System.out.println("Hello,git");
+    }
+}
